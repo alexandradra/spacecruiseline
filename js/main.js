5ec26b5eb9031e110806a496d3fe805ac8e2ac2c
Alexandra Dubost <alexandra.dubost@gmail.com>
2017-11-05 19:05:34 +0100

    Nouveau menu + logo / Slider OK / Début form résa

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   $('.slider').bxSlider({
-    mode: 'fade',
-    captions: false,
-    tickerHover: true
+    adaptiveHeight:true,
+    speed:1500,
+    pause:7000000,
+    pager:false,
+    auto: true,
+    mode:'fade',
+    controls: true,
   });
 });
